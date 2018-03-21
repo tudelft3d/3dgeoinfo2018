e2c5d490a7be54a3ccb1b80d8b78470a57007c86
Ken Arroyo Ohori <g.a.k.arroyoohori@tudelft.nl>
2018-03-21 16:19:50 -0600

    Update script

---
+++ update/index.php
@@ -0,0 +1,25 @@
+---
+layout: default
+---
+
+<div class="container">
+<?php
+	$repository_path = '/var/www/3dgeoinfo';
+?>
+
+<h1>Git pull</h1>
+<pre><code>
+<?php
+	echo("$ git -C ".$repository_path." pull\n");
+	system("git -C ".$repository_path." pull 2>&1");
+?>
+</code></pre>
+
+<h1>Jekyll build</h1>
+<pre><code>
+<?php
+	echo("$ jekyll build --source ".$repository_path." --destination ".$repository_path."/_site\n");
+	system("jekyll build --source ".$repository_path." --destination ".$repository_path."/_site 2>&1");
+?>
+</code></pre>
+</div>
